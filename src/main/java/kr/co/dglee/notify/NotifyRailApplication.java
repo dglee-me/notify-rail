63e2ef778979b84e239a5f7f49540bb65ae80801
dglee-me <dglee.dev@gmail.com>
2026-06-09 21:56:01 +0900

    init

---
+++ src/main/java/kr/co/dglee/notify/NotifyRailApplication.java
@@ -0,0 +1,13 @@
+package kr.co.dglee.notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyRailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotifyRailApplication.class, args);
+    }
+
+}
